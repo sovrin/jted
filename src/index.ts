652fe696d9389d612c0caa18a5cb333477c204b1
Oleg Kamlowski <kamlowski.oleg@gmail.com>
2022-11-12 21:18:05 +0100

    refactor: index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import sign from './sign';
-import decode from './decode';
-import encode from './encode';
-
 /**
  * User: Oleg Kamlowski <oleg.kamlowski@thomann.de>
  * Date: 16.12.2020
  * Time: 17:56
  */
-export {
-    sign,
-    decode,
-    encode,
-};
+export {default as verify} from './verify';
+export {default as decode} from './decode';
+export {default as encode} from './encode';
